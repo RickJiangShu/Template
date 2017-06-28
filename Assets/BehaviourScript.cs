343d2bc573f098418deedc9972917d93dce367cb
Rick <rickjiangshu@gmail.com>
2017-06-28 18:12:01 +0800

    Initial commit

---
+++ Assets/BehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Author:  Rick
+ * Date:    $time$
+ * Email:   rickjiangshu@gmail.com
+ * Follow:  https://github.com/RickJiangShu
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// $itemname$
+/// </summary>
+public class $itemname$ : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Editor/EditorScript.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Author:  Rick
+ * Date:    $time$
+ * Email:   rickjiangshu@gmail.com
+ * Follow:  https://github.com/RickJiangShu
+ */
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// $itemname$
+/// </summary>
+public class $itemname$ : ScriptableObject
+{
+    [MenuItem("Tools/MyTool/Do It in C#")]
+    static void DoIt()
+    {
+        EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
+    }
+}
